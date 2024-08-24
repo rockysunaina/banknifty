8a36f42dd213e6b5112e8ff7ff4f9b78b37b4ef2
rockysunaina <93891944+rockysunaina@users.noreply.github.com>
2024-08-24 12:15:58 +0530

    Update fetch_data.php

--- fetch_data.php
+++ fetch_data.php
@@ -2,37 +2,33 @@
 // Execute the Python script
 exec("python3 fetch_banknifty_data.py");
 
-// Zip the directory containing the fetched data
-$zip = new ZipArchive();
-$filename = "banknifty_data.zip";
-
-if ($zip->open($filename, ZipArchive::CREATE) !== TRUE) {
-    exit("Cannot open <$filename>\n");
-}
-
+// Find the latest JSON file created by the Python script
 $dir = 'banknifty/';
-
 $files = new RecursiveIteratorIterator(
     new RecursiveDirectoryIterator($dir),
     RecursiveIteratorIterator::LEAVES_ONLY
 );
 
+$latest_file = '';
+$latest_time = 0;
+
 foreach ($files as $file) {
-    if (!$file->isDir()) {
-        $filePath = $file->getRealPath();
-        $relativePath = substr($filePath, strlen($dir));
-        $zip->addFile($filePath, $relativePath);
+    if ($file->isFile()) {
+        $file_time = $file->getMTime();
+        if ($file_time > $latest_time) {
+            $latest_time = $file_time;
+            $latest_file = $file->getRealPath();
+        }
     }
 }
 
-$zip->close();
-
-// Serve the zip file for download
-header('Content-Type: application/zip');
-header('Content-disposition: attachment; filename=' . $filename);
-header('Content-Length: ' . filesize($filename));
-readfile($filename);
-
-// Clean up the zip file after download
-unlink($filename);
+if ($latest_file) {
+    // Serve the latest JSON file for download
+    header('Content-Type: application/json');
+    header('Content-Disposition: attachment; filename=' . basename($latest_file));
+    header('Content-Length: ' . filesize($latest_file));
+    readfile($latest_file);
+} else {
+    echo "No data available for download.";
+}
 ?>
